b970286e25ef55a7cf8bdeb8e506f875d2a7c673
januwA <ajanuw1641845087@sina.com>
2019-09-28 12:00:37 +0800

    预览去重复项

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { PreviewImage } from 'src/app/shared/interfaces/preview-image.interface';
-import { UploadService } from './upload.service';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {PreviewImage} from 'src/app/shared/interfaces/preview-image.interface';
+import {UploadService} from './upload.service';
 import * as _ from 'lodash';
 
 @Component({
@@ -10,58 +10,67 @@ import * as _ from 'lodash';
   styleUrls: ['./upload.component.styl'],
 })
 export class UploadComponent implements OnInit {
-  public files: PreviewImage[];
+  public files: PreviewImage[] = [];
 
   constructor(
     private sanitizer: DomSanitizer,
     private uploadService: UploadService,
-  ) { }
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngOnDestroy() {
-    if (this.files) {
-      _(this.files).forEach(file => {
-        window.URL.revokeObjectURL(file.src);
-      });
-    }
-  }
-  hadler(e) {
-    e.preventDefault();
-  }
-  dropEvent(e) {
-    e.preventDefault();
-    const files = [];  //e.target.files 返回的值为空，换一种方式来获取File
-    [].forEach.call(e.dataTransfer.files, function (file) {
-      files.push(file);
-    }, false);
-    let _files: any[] = Array.from(files);
-    _files = _files.map<PreviewImage>(file => {
-      const src = window.URL.createObjectURL(
-        new Blob([file], {
-          type: file.type,
-        }),
-      );
-
-      return {
-        // see also:  https://angular.io/guide/security#xss
-        src: this.sanitizer.bypassSecurityTrustUrl(src),
-        file,
-      };
+    _(this.files).forEach((file: PreviewImage) => {
+      window.URL.revokeObjectURL(file.src);
     });
-
-    if (this.files) {
-      this.files.push(..._files);
-    } else {
-      this.files = _files;
-    }
-    this.uploadAll();
   }
 
-  onFileChange(e) {
+  hadler(e: any) {
+    e.preventDefault();
+  }
+
+  /**
+   * * 拖拽时获取图片
+   * @param e
+   */
+  dropEvent(e: any) {
+    e.preventDefault();
+    this._setFiles(e.dataTransfer.files);
+  }
+
+  /**
+   * * 点击上传获取图片
+   * @param e
+   */
+  onFileChange(e: any) {
     if (0 > e.target.files) return null;
-    let _files: any[] = Array.from(e.target.files);
-    _files = _files.map<PreviewImage>(file => {
+    this._setFiles(e.target.files);
+  }
+
+  /**
+   * * 记录图片
+   * @param files
+   */
+  private _setFiles(files: any[]) {
+    let _files: PreviewImage[] = this._createPreviewImages(files);
+
+    // 每次上传，将功率掉重复项，用[name]过滤
+    this.files = _.unionWith<PreviewImage>(
+      this.files,
+      _files,
+      (a: PreviewImage, b: PreviewImage) => {
+        return a.file.name === b.file.name;
+      },
+    );
+  }
+
+  /**
+   * 使用 file[] 创建 PreviewImage[]
+   * @param files fileList
+   */
+  private _createPreviewImages(files: any[]): PreviewImage[] {
+    // 避免假数组
+    return _.map(_.toArray(files), file => {
       const src = window.URL.createObjectURL(
         new Blob([file], {
           type: file.type,
@@ -72,14 +81,8 @@ export class UploadComponent implements OnInit {
         // see also:  https://angular.io/guide/security#xss
         src: this.sanitizer.bypassSecurityTrustUrl(src),
         file,
-      };
+      } as PreviewImage;
     });
-
-    if (this.files) {
-      this.files.push(..._files);
-    } else {
-      this.files = _files;
-    }
   }
 
   /**
@@ -92,15 +95,25 @@ export class UploadComponent implements OnInit {
   /**
    * * 删除预览图片
    */
-  deletePreviewImage(file) {
-    this.files = this.files.filter(f => f !== file);
+  deletePreviewImage(file: PreviewImage) {
+    _.pull(this.files, file);
   }
 
   /**
-   * * 上传所有的图片
+   * * 点击[全部上传]，上传所有的图片
+   * * 上传完毕清理预览图片
+   * TODO: 大型图片上传失败，但还是提示上传成功
    */
   async uploadAll() {
-    await this.uploadService.upload(this.files.map(f => f.file));
+    await this.uploadService.upload(_.toArray(this.files).map(f => f.file));
     alert('全部已上传...');
+    this.clearPreviewImages();
+  }
+
+  /**
+   * * 清理所有预览图片
+   */
+  clearPreviewImages() {
+    this.files = [];
   }
 }
